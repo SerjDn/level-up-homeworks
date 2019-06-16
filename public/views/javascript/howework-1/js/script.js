e8e0f83a08e3ed1f51833716fbcbcd7b667c8900
SerjDn <serj388@gmail.com>
2019-06-16 13:29:53 +0300

    JS HW

---
+++ public/views/javascript/howework-1/js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Task 1
+var admin;
+var name;
+name = 'Василий';
+admin = name;
+console.log(admin);
+
+// Task 2
+var ourPlanetName;
+ourPlanetName = 'Земля';
+var user;
+user = {
+  name: 'Петя',
+};
+
+// Additional task 1
+var framesPerSecond;
+var connectingSignBetweenWords;
+var viewDocumentMode;
+var showLastPageNavigation;
