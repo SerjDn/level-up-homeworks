5f4db2c2ef2cf085e60a8e65006df113531a11f6
SerjDn <serj388@gmail.com>
2019-06-21 00:08:52 +0300

    JS HW Update

--- public/views/javascript/howework-1/js/script.js
+++ public/views/javascript/howework-1/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Unit link - https://learn.javascript.ru/variables#tasks
 // Task 1
 var admin;
 var name;
@@ -7,16 +8,13 @@ name = 'Василий';
 admin = name;
 console.log(admin);
 
+// Unit link - https://learn.javascript.ru/variable-names#tasks
 // Task 2
-var ourPlanetName;
-ourPlanetName = 'Земля';
-var user;
-user = {
-  name: 'Петя',
-};
+var ourPlanetName = 'Земля';
+var userName = 'Петя';
 
 // Additional task 1
-var framesPerSecond;
-var connectingSignBetweenWords;
-var viewDocumentMode;
-var showLastPageNavigation;
+var fps;
+var connectorBetweenWords;
+var documentView;
+var showLastPageNav;
