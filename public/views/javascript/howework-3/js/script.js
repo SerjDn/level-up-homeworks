396335a144e8d00322047f415c6d64fad8c62bdf
SerjDn <serj388@gmail.com>
2019-06-28 00:09:47 +0300

    JS HW 3

--- public/views/javascript/howework-3/js/script.js
+++ public/views/javascript/howework-3/js/script.js
@@ -8,29 +8,29 @@ if ('0') {
 
 // Unit link - https://learn.javascript.ru/task/check-standard
 // Task 2
-// var JSNname = prompt('Каково официальное название JavaScript?');
+var JSNname = prompt('Каково официальное название JavaScript?');
 
-// if (JSNname === 'ECMAScript' || JSNname === 'EcmaScript' || JSNname === 'ecmascript') {
-//   alert('Good!');
-// } else {
-//   alert('Не знаете? "ECMAScript"!');
-// }
+if (JSNname === 'ECMAScript' || JSNname === 'EcmaScript' || JSNname === 'ecmascript') {
+  alert('Good!');
+} else {
+  alert('Не знаете? "ECMAScript"!');
+}
 // тут вопрос - почему не срабатывает следующие формы записи в conditions?
 // 1) if (JSNname === 'ECMAScript' || 'EcmaScript' || 'ecmascript')
 // 2) if (JSNname === 'ECMAScript', 'EcmaScript', 'ecmascript')
 
 // Unit link - https://learn.javascript.ru/task/sign
 // Task 3
-// var sign = prompt('Введите сюда что-то:');
+var sign = prompt('Введите сюда что-то:');
 
-// if (sign > 0) {
-//   alert(1);
-// } else if (sign < 0) {
-//   alert(-1);
-// }
-// else {
-//   alert(0);
-// }
+if (sign > 0) {
+  alert(1);
+} else if (sign < 0) {
+  alert(-1);
+}
+else {
+  alert(0);
+}
 
 // Unit link - https://learn.javascript.ru/task/check-login
 // Task 4
@@ -46,6 +46,14 @@ else {
   alert('Ты не админ!');
 }
 
-// Unit link - https://learn.javascript.ru/task/check-login
+// Unit link - https://learn.javascript.ru/task/rewrite-if-question
 // Task 5
+var a = 1;
+var b = 2;
+var result = (a + b < 4) ? 'Мало' : 'Много';
+console.log(result); // в консоль выводится "Мало", т.к. 2 +1 = 3 и это < 4
 
+// Unit link - https://learn.javascript.ru/task/rewrite-if-else-question
+// Task 6
+var login = prompt('Укажите Ваш логин')
+var message = (login === 'Вася') ? alert('Привет') : (login === 'Директор') ? alert('Здравствуйте') : (login === ' ') ? alert('Нет логина') : alert('');
